6354687a361f8c27f590a107eec74a706430401a
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-04-24 11:02:49 +0100

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="payment-app">
+    <h5 class="text-center py-4">Enter Payment Information</h5>
+    <form id="form">
+      <div class="field-wrapper position-relative">
+        <input type="text" placeholder="Email or phone number" class="default-input"/>
+        <img src="@/assets/img/email.png" width="25" />
+      </div>
+      <div class="mt-4">
+        <input type="text" placeholder="Card number" class="default-input"/>
+      </div>
+      <div class="mt-4">
+        <input type="text" placeholder="Expiration date" class="default-input"/>
+      </div>
+      <div class="mt-4">
+        <input type="number" placeholder="CVV" class="default-input"/>
+      </div>
+      <div class="mt-4">
+        <button class="default-button w-100" type="submit">Submit </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/sass/styles.scss";
 </style>
